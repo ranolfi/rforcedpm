f7b46b0cd8f6cfb5f38448fde9e860ad6c9d9e18
ranolfi <mranolfi@gmail.com>
2018-04-14 02:38:50 -0300

    Add toggle support for the <auto> power state which is the driver default.

--- rforcedpm.sh
+++ rforcedpm.sh
@@ -6,25 +6,41 @@
 #
 # Inspired by a bug with Radeon RX 550 driver <https://bugzilla.redhat.com/show_bug.cgi?id=1478219>
 #
+# Caveat: Assumes a single videocard is installed in the system.
+#
 
-if cat /sys/class/drm/card0/device/power_dpm_force_performance_level | grep -qs high
+STATE=$(cat /sys/class/drm/card0/device/power_dpm_force_performance_level)
+
+echo -e "DPM status is <$STATE>.\n"
+
+echo "Select an option, or press [Enter] to quit."
+
+if [[ $STATE == "auto" ]]
 then
-  echo -e "DPM status is <High>.\n"
-  echo "Switch to <Low>? [y/N] "
-
-  read response
-  if [[ $response == "y" ]]
-  then
-    sudo sh -c "echo 'low' > /sys/class/drm/card0/device/power_dpm_force_performance_level"
-  fi
-
+  echo "1. <low>"
+  echo "2. <high>"
+elif [[ $STATE == "low" ]]
+then
+  echo "1. <auto>"
+  echo "2. <high>"
 else
-  echo -e "DPM status is <Low>.\n"
-  echo "Switch to <High>? [y/N] "
+  echo "1. <auto>"
+  echo "2. <low>"
+fi
 
-  read response
-  if [[ $response == "y" ]]
-  then
-    sudo sh -c "echo 'high' > /sys/class/drm/card0/device/power_dpm_force_performance_level"
-  fi
+read RESPONSE
+CHANGE_TO=""
+
+if [[ $STATE == "auto" ]] && [[ $RESPONSE == "1" ]]; then CHANGE_TO="low"
+elif [[ $STATE == "auto" ]] && [[ $RESPONSE == "2" ]]; then CHANGE_TO="high"
+elif [[ $STATE == "low" ]] && [[ $RESPONSE == "1" ]]; then CHANGE_TO="auto"
+elif [[ $STATE == "low" ]] && [[ $RESPONSE == "2" ]]; then CHANGE_TO="high"
+elif [[ $STATE == "high" ]] && [[ $RESPONSE == "1" ]]; then CHANGE_TO="auto"
+elif [[ $STATE == "high" ]] && [[ $RESPONSE == "2" ]]; then CHANGE_TO="low"
+fi
+
+if [ ! -z "$CHANGE_TO" ]
+then
+  sudo sh -c "echo '$CHANGE_TO' > /sys/class/drm/card0/device/power_dpm_force_performance_level"
+  echo "DPM status set to <$CHANGE_TO>."
 fi
